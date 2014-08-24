34c51ddae1e0bbd35a0eff38a3ddf6bf00ec0323
George Lifchits <george.lifchits@gmail.com>
2014-08-23 23:58:07 -0400

    stuff for Karma testing WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,46 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var karma = require('gulp-karma');
 var watch = require('gulp-watch');
 var connect = require('gulp-connect');
 
 
+var testFiles = [
+	'js/app.js',
+	'test/spec/**/*.js'
+];
+
+
 gulp.task('css', function() {
-	watch({ glob: 'scss/*.scss' })
-		.pipe(sass())
-		.pipe(gulp.dest('css'))
-		.pipe(connect.reload());
+	watch({ glob: 'scss/*.scss' }, function(files) {
+		return files.pipe(sass())
+			.pipe(gulp.dest('css'))
+			.pipe(connect.reload());
+	});
+});
+
+
+gulp.task('test', function() {
+	return gulp.src(testFiles)
+		.pipe(karma({
+			configFile: 'karma.conf.js',
+			action: 'run'
+		}))
+		.on('error', function(err) {
+			throw err;
+		});
 });
 
 
 gulp.task('server', function() {
-	connect.server({ livereload: true });
+	return connect.server({ livereload: true });
 });
 
 
-gulp.task('default', ['server', 'css']);
+gulp.task('default', ['server', 'css'], function() {
+	gulp.src(testFiles)
+		.pipe(karma({
+			configFile: 'karma.conf.js',
+			action: 'watch'
+		}));
+});
---
+++ test/spec/controllers/calculator.js
@@ -0,0 +1,56 @@
+describe('Unit: CalculatorController', function() {
+	// Load the module with CalculatorController
+	beforeEach(module('gpaCalc'));
+
+	var ctrl, scope;
+	beforeEach(inject(function($controller, $rootScope) {
+		scope = $rootScope.$new();
+		ctrl = $controller('CalculatorController', {
+			$scope: scope
+		});
+	}));
+
+	it('should have cumu == starting if no credits are entered', function() {
+		expect(scope.cumu.gpa).toEqual(0);
+		expect(scope.credits.length).toEqual(0);
+		scope.start.gpa = 9.4;
+		scope.start.credits = 20.5;
+		expect(scope.cumu.gpa).toEqual(9.4);
+		expect(scope.cumu.credits).toEqual(20.5);
+	});
+
+	it('should update cumu as credits are entered', function() {
+		expect(scope.cumu.gpa).toEqual(0);
+		scope.credits.push({
+			grade: 10,
+			credits: 0.5
+		});
+		expect(scope.cumu.gpa).toEqual(10);
+		expect(scope.cumu.credits).toEqual(0.5);
+		scope.credits.push({
+			grade: 8,
+			credits: 0.5
+		});
+		expect(scope.cumu.gpa).toEqual(9);
+		expect(scope.cumu.credits).toEqual(1);
+		scope.credits.push({
+			grade: 12,
+			credits: 1.0 
+		});
+		expect(scope.cumu.gpa).toEqual(10.5);
+		expect(scope.cumu.credits).toEqual(2);
+	});
+
+	it('should consider starting GPA when calculating cumu.gpa', function() {
+		expect(scope.cumu.gpa).toEqual(0);
+		scope.start.gpa = 10;
+		scope.start.credits = 0.5;
+		expect(scope.cumu.gpa).toEqual(10);
+		scope.credits.push({
+			grade: 8,
+			credits: 0.5
+		});
+		expect(scope.cumu.gpa).toEqual(9);
+		expect(scope.cumu.credits).toEqual(1);
+	});
+});
